3b6c53ce9e9cd15ef39215c881bc97b083678376
Arnaud Lefevre <rnonet@gmail.com>
2011-11-30 17:22:09 +0100

    Villaret

---
+++ test.php
@@ -0,0 +1,376 @@
+<html>
+<head>
+<link rel="stylesheet" type= "text/css" href="style1.css">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<script language="JavaScript">
+var Villaret = 
+{
+	Intitule: {
+		fr: "Français",
+		en: "English"
+	},
+	LOYER: {
+		fr: "Loyer",
+		en: "Rental"
+	},
+	TYPE_MANDAT: {
+		fr: "Type de mandat",
+		en: "Type of warrant"
+	},
+	FRAIS_AGENCE: {
+		fr: "Frais d'agence",
+		en: "Agency fees"
+	},
+	DEPOT_GARANTIE: {
+		fr: "Dépot de garantie",
+		en: "Security deposit"
+	},
+	PROVISIONS_SUR_CHARGES: {
+		fr: "Provisions sur charges",
+		en: "Provisions for charges"
+	},
+	NUM_MANDAT: {
+		fr: "Numéro de mandat",
+		en: "Warrant number"
+	},
+	TYPE_MANDAT: {
+		fr: "Type de mandat",
+		en: "Type of warrant"
+	}, 
+	DATE_MANDAT: {
+		fr: "Date du mandat",
+		en: "Date of warrant"
+	},
+	PRIX: {
+		fr: "Prix",
+		en: "Price"
+	},
+	CHARGES_MENSUELLES: {
+		fr: "Charges mensuelles",
+		en: "Monthly fees"
+	}, 
+	TAXE_FONCIERE: {
+		fr: "Taxe foncière",
+		en: "Estate tax"
+	}, 
+	TAXE_HABITATION: {
+		fr: "Taxe d'habitation",
+		en: "Housing tax"
+	}, 
+	SS_TYPE: {
+		fr: "Sous-type",
+		en: "Subtype"
+	}, 
+	CATEGORIE_MERE: {
+		fr: "Catégorie mère",
+		en: "Parent category"
+	},
+	CPVILLE: {
+		fr: "Code postal",
+		en: "Zip code"
+	}, 
+	NBRE_PIECES: {
+		fr: "Nombre de pièces",
+		en: "Number of rooms"
+	},
+	NBRE_CHAMBRES: {
+		fr: "Nombre de Chambres",
+		en: "Number of rooms"
+	},
+	SURFACE_HABITABLE: {
+		fr: "Surface habitable",
+		en: "Living"
+	}, 
+	SURFACE_SEJOUR: {
+		fr: "Surface du séjour",
+		en: "Living surface"
+	}, 
+	SURFACE_TERRAIN: {
+		fr: "Surface du terrain",
+		en: "Ground surface"
+	}, 
+	SHON: {
+		fr: "Surface hors oeuvre nette",
+		en: " "
+	}, 
+	FACADE: {
+		fr: "Façade",
+		en: "Facade"
+	}, 
+	PROFONDEUR: {
+		fr: "Profondeur",
+		en: "Depth"
+	}, 
+	NBRE_NIVEAUX: {
+		fr: "Nombre de niveaux",
+		en: "Number of levels"
+	}, 
+	ANNEE_CONSTRUCTION: {
+		fr: "Année de construction",
+		en: "Year of built"
+	}, 
+	CATEGORIE: {
+		fr: "Catégorie",
+		en: "Category"
+	}, 
+	NBRE_SALLE_BAIN: {
+		fr: "Nombre de salles de bain",
+		en: "Number of bathrooms"
+	}, 
+	ETAT_GENERAL: {
+		fr: "Etat général",
+		en: "General statement"
+	}, 
+	NBRE_BALCON: {
+		fr: "Nombre de balcons",
+		en: "Number of balconies"
+	}, 
+	NBRE_GARAGE: {
+		fr: "Nombre de garages",
+		en: "Number of garages"
+	}, 
+	NBRE_PARKING: {
+		fr: "Nombre de parkings",
+		en: "Number of park"
+	}, 
+	NBRE_SALLE_EAU: {
+		fr: "Nombre de salles d'eau",
+		en: "Number of showerooms"
+	}, 
+	NBRE_CAVES: {
+		fr: "Nombre de caves",
+		en: "Number of caves"
+	}, 
+	CUISINE: {
+		fr: "Cuisine",
+		en: "Kitchen"
+	}, 
+	SOUS_SOL:{
+		fr: "Sous-sol",
+		en: "Basement"
+	}, 
+	CONSTRUCTION: {
+		fr: "Construction",
+		en: "Construction"
+	}, 
+	STANDING: {
+		fr: "Standing",
+		en: "Standing"
+	}, 
+	MITOYENNETE: {
+		fr: "Mitoyenneté",
+		en: "Join ownership"
+	}, 
+	MEUBLE: {
+		fr: "Meublé",
+		en: "Furnished"
+	}, 
+	COURVERTURE: {
+		fr: "Courverture",
+		en: "Coverage"
+	}, 
+	EXPOSITION: {
+		fr: "Exposition",
+		en: "Exposure"
+	}, 
+	CHAUFFAGE: {
+		fr: "Chauffage",
+		en: "Heating"
+	}, 
+	MECANISME_CHAUFFAGE: {
+		fr: "Mécanisme de chauffage",
+		en: "Heating mechanism"
+	}, 
+	MODE_CHAUFFAGE: {
+		fr: "Mode de chauffage",
+		en: "Heating type"
+	} , 
+	CONSOMMATIONENERGETIQUE: {
+		fr: "Consommation énergétique",
+		en: "Energy consumption"
+	} , 
+	GAZEFFETDESERRE: {
+		fr: "Gaz à effet de serre",
+		en: "Greenhouse gases"
+	} , 
+	CONSO_ANNUEL_ENERGIE: {
+		fr: "Consommation annuelle d'énergie", 
+		en: "Annual energy consumption"
+	} , 
+	VALEUR_GES: {
+		fr: "Valeur Ges",
+		en: "Valeur Ges"
+	} , 
+	PICTOGRAMME: {
+		fr: "Pictogramme",
+		en: "Picture"
+	} , 
+	NUM_ETAGE: {
+		fr: "Numéro d'étage",
+		en: "Floor number"
+	} , 
+	NUM_DERNIER_ETAGE: {
+		fr: "numéro du dernier étage", 
+		en: "Number of last floor"
+	} , 
+	EAU_CHAUDE: {
+		fr: "Eau chaude",
+		en: "Hot water"
+	} , 
+	ACCES_HANDICAPES: {
+		fr: "Accès handicapés",
+		en: "Disabled acces"
+	} , 
+	ASCENSEUR: {
+		fr: "Ascenseur",
+		en: "Elevator"
+	} , 
+	SELECTION_PAGE_ACCUEIL: {
+		fr: "Sélectionner comme page d'accueil",
+		en: "Select as homepage"
+	} , 
+	REGIME_FISCAL: {
+		fr: "Régime fiscal",
+		en: "Taxes"
+	} , 
+	TYPE_RESIDENCE: {
+		fr: "Type de résidence",
+		en: "Type of residence"
+	} , 
+	NBRE_TERRASSE: {
+		fr: "Nombre de terrasses",
+		en: "Number of terraces"
+	} , 
+	SURFACE_MINIMUM: {
+		fr: "Surface minimale",
+		en: "Minimum area"
+	} , 
+	SURFACE_DIVISIBLE: {
+		fr: "Surface divisible",
+		en: "Divided surface"
+	} , 
+	LONGUEUR_VITRINE: {
+		fr: "Longueur de vitrine",
+		en: "Window length"
+	} , 
+	ACTIVITE: {
+		fr: "Activité",
+		en: "Activity"
+	} , 
+	SURFACE_TOTALE: {
+		fr: "Surface totale",
+		en: "Global area"
+	} , 
+	CODE_POSTAL: {
+		fr: "Code postal",
+		en: "Zip code"
+	} , 
+	VILLE: {
+		fr: "Ville",
+		en: "City"
+	} , 
+	PAYS: {
+		fr: "Pays",
+		en: "Country"
+	} , 
+	INSEE: {
+		fr: "INSEE",
+		en: "INSEE"
+	} , 
+	LATITUDE: {
+		fr: "Latitude",
+		en: "Latitude"
+	} , 
+	LONGITUDE: {
+		fr: "Longitude",
+		en: "Longitude"
+	} , 
+	SECTEUR: {
+		fr: "Secteur",
+		en: "District"
+	} , 
+	SITUATION: {
+		fr: "Situation",
+		en: "Position"
+	} , 
+	NOM_RESIDENCE: {
+		fr: "Nom de résidence",
+		en: "Residence name"
+	} , 
+	GIRARDIN: {
+		fr: "GIRARDIN",
+		en: "GIRARDIN" 
+	} , 
+	BORLOO: {
+		fr: "BORLOO",
+		en: "BORLOO"
+	} , 
+	DEROBIEN: {
+		fr: "DEROBIEN",
+		en: "DEROBIEN"
+	} , 
+	SCELLIER: {
+		fr: "SCELLIER",
+		en: "SCELLIER"
+	} , 
+	LMP: {
+		fr: "LMP",
+		en: "LMP"
+	} , 
+	ZRR: {
+		fr: "ZRR",
+		en: "ZRR"
+	}
+};
+
+
+function monTableau (choix)
+{
+var body  = document.getElementsByTagName("body")[0];
+table     = document.createElement("table");
+tablebody = document.createElement("tbody");
+var monCss = 0;
+for (val in Villaret){
+//document.write(choix);
+     for(var j = 0; j<1; j++) {
+        var row = document.createElement("tr");
+        monCss++;
+        for(var i = 0; i < 1; i++ ) {
+			cell = document.createElement("td");
+			texte = document.createTextNode(val);
+			cell.appendChild(texte);
+			row.appendChild(cell);
+         	}
+        for(var i = 0; i < 1; i++ ) {
+			cell = document.createElement("td");
+			texte = document.createTextNode(Villaret[val].fr);
+			cell.appendChild(texte);
+			row.appendChild(cell);
+         	}
+        for(var i = 0; i < 1; i++ ) {
+			cell = document.createElement("td");
+			texte = document.createTextNode(Villaret[val].en);
+			cell.appendChild(texte);
+			row.appendChild(cell);
+         	}
+		tablebody.appendChild(row);
+     	}
+     table.appendChild(tablebody);
+     body.appendChild(table);
+     table.setAttribute("id", "tableau");
+     	if (monCss==1){row.setAttribute("id", "tr0");}
+    	else {
+     		if (monCss%2==0){row.setAttribute("id", "tr1");}
+     		else {row.setAttribute("id", "tr2");}
+     	}
+     }
+}
+</script>
+</head>
+<body id="corps">
+<script language="JavaScript">
+monTableau ();
+</script>
+</body>
+</html>
+
